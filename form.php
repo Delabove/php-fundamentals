2214d62f74e91166e6ab77b6d160f59200438839
DeLayne <delayne.labove@gmail.com>
2021-03-20 16:42:55 -0500

    form validation

--- form.php
+++ form.php
@@ -14,45 +14,67 @@ $tc='';
 
 if(isset($_POST['submit'])) {
 //    echo htmlspecialchars($_POST['searchterm'], ENT_QUOTES);
-     if(isset($_POST['name'])){
-         $name= $_POST['name'];
-     }
-    if(isset($_POST['password'])){
-        $password= $_POST['password'];
-    }
-    if(isset($_POST['gender'])){
-        $gender= $_POST['gender'];
-    }
-    if(isset($_POST['color'])){
-        $color= $_POST['color'];
-    }
-    if(isset($_POST['languages'])){
-        $languages= $_POST['languages'];
-    }
-    if(isset($_POST['comments'])){
-        $comments= $_POST['comments'];
-    }
-    if(isset($_POST['tc'])){
-        $tc= $_POST['tc'];
-    }
+    $ok = true;
+
+        if (!isset($_POST['name']) || $_POST['name'] === '') {
+            $ok = false;
+        } else {
+            $name = $_POST['name'];
+        };
+
+        if (!isset($_POST['password']) || $_POST['password'] === '') {
+            $ok = false;
+        } else {
+            $password = $_POST['password'];
+        };
+        if (!isset($_POST['gender']) || $_POST['gender'] === '') {
+            $ok = false;
+        } else {
+            $gender = $_POST['gender'];
+        };
+        if (!isset($_POST['color']) || $_POST['color'] === '') {
+            $ok = false;
+        } else {
+            $color = $_POST['color'];
+        };
+        if (!isset($_POST['languages']) || !is_array($_POST['languages']) || count($_POST['languages']) == 0) {
+            $ok = false;
+        } else {
+            $languages = $_POST['languages'];
+        };
+        if (!isset($_POST['comments']) || $_POST['comments'] === '') {
+            $ok = false;
+        } else {
+            $comments = $_POST['comments'];
+        };
+        if (!isset($_POST['tc']) || $_POST['tc'] === '') {
+            $ok = false;
+        } else {
+            $tc = $_POST['tc'];
+        }
 
     // output and escape everything with the html escape for security
+    if($ok) {
 
-    printf('Username: %s
+        printf('Username: %s
          <br>Password: %s
          <br>Gender: %s
          <br>Color: %s
          <br>Languages: %s
          <br>Comments: %s
          <br>T&amp;C: %s',
-        htmlspecialchars($name, ENT_QUOTES),
-        htmlspecialchars($password, ENT_QUOTES),
-        htmlspecialchars($gender, ENT_QUOTES),
-        htmlspecialchars($color, ENT_QUOTES),
-        htmlspecialchars(implode('', $languages), ENT_QUOTES),
-        htmlspecialchars($comments, ENT_QUOTES),
-        htmlspecialchars($tc, ENT_QUOTES));
+            htmlspecialchars($name, ENT_QUOTES),
+            htmlspecialchars($password, ENT_QUOTES),
+            htmlspecialchars($gender, ENT_QUOTES),
+            htmlspecialchars($color, ENT_QUOTES),
+            htmlspecialchars(implode('', $languages), ENT_QUOTES),
+            htmlspecialchars($comments, ENT_QUOTES),
+            htmlspecialchars($tc, ENT_QUOTES));
+
+//        echo 'true';
     }
+}
+
     ?>
 
 <h1>Registration form</h1>
@@ -80,9 +102,8 @@ if(isset($_POST['submit'])) {
         <option value="it">Italian</option>
     </select> <br> <br>
     Comments: <textarea name="comments" ></textarea><br><br>
-<!--
- <input type="checkbox" name="tc" value="ok">-->
-<!--    I accept the T&amp;C <br><br>--><!--    I accept the T&amp;C <br><br>-->
+    <input type="checkbox" name="tc" value="ok">
+    I accept the T&amp;C <br><br>
     <input type="submit" name="submit" value="SUBMIT">
 
 </form>
