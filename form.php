01a4f5fb89e3d8549561bbd2549a486058a9cbfa
DeLayne <delayne.labove@gmail.com>
2021-04-03 20:30:48 -0500

    prefill list fields

--- form.php
+++ form.php
@@ -103,15 +103,39 @@ if(isset($_POST['submit'])) {
     Favorite Color:
     <select name="color" id="">
         <option value="">Please Select</option>
-        <option value="#f00">Red</option>
-        <option value="#0f0">Green</option>
-        <option value="#00f">Blue</option>
-    </select> <br> <br>
+        <option value="#f00"<?php
+        if($color == '#f00'){
+            echo ' selected';
+        }
+        ?>>Red</option>
+        <option value="#0f0"<?php
+        if($color == '#0f0'){
+            echo ' selected';
+        }
+        ?>>Green</option>
+        <option value="#00f"<?php
+        if($color == '#00f'){
+            echo ' selected';
+        }
+        ?>>Blue</option>
+    </select> <br><br>
     Languages Spoken:
     <select name="languages[]" multiple size="3">
-        <option value="en">English</option>
-        <option value="fr">French</option>
-        <option value="it">Italian</option>
+        <option value="en"<?php
+        if(in_array('en', $languages)){
+            echo ' selected';
+        }
+        ?>>English</option>
+        <option value="fr"<?php
+        if(in_array('fr', $languages)){
+            echo ' selected';
+        }
+        ?>>French</option>
+        <option value="it"<?php
+        if(in_array('it', $languages)){
+            echo ' selected';
+        }
+        ?>>Italian</option>
     </select> <br> <br>
     Comments: <textarea name="comments" > <?php echo htmlspecialchars($comments, ENT_QUOTES)?></textarea><br><br>
     <input type="checkbox" name="tc" value="ok"<?php
