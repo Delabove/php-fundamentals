7ef3f227dd52444c6ab1447d8397f32fe0d8177b
DeLayne <delayne.labove@gmail.com>
2021-04-08 20:34:33 -0500

    delete error handling

--- delete.php
+++ delete.php
@@ -11,8 +11,16 @@ if(isset($_GET['id']) && ctype_digit($_GET['id']))
 include 'db_connection.php';
 
 $conn = OpenCon();
-$sql = 'DELETE from users WHERE id=$id ';
+$sql = "DELETE FROM users WHERE id=$id ";
 $conn->query($sql);
+
+if (mysqli_query($conn, $sql)) {
 echo 'User has been deleted';
 
+} else {
+    echo "Error deleting record: " . mysqli_error($conn);
+}
+
+
 CloseCon($conn);
+?>
