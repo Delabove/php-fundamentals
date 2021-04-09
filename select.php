b4c1d476a2efacd6521ca8f7707568a08da0d988
DeLayne <delayne.labove@gmail.com>
2021-04-08 21:52:21 -0500

    added update but form not currently prepopulating with the users information to be updated fix tomorrow

--- select.php
+++ select.php
@@ -2,7 +2,7 @@
 <ul>
     <?php
     include 'db_connection.php';
-
+    error_log("There is something wrong!", 0);
     $conn = OpenCon();
 
     $sql = 'SELECT * FROM users';
@@ -13,10 +13,12 @@
         printf(
           '<li><span style="color:%s">%s (%s)</span>
 <a href="delete.php?id=%s">delete</a>
+<a href="update.php?id=%s">update</a>
 </li>',
             htmlspecialchars($row['color'], ENT_QUOTES),
             htmlspecialchars($row['name'], ENT_QUOTES),
             htmlspecialchars ($row['gender'], ENT_QUOTES),
+            htmlspecialchars ($row['id'], ENT_QUOTES),
             htmlspecialchars ($row['id'], ENT_QUOTES));
 
     }
--- update.php
+++ update.php
@@ -3,6 +3,14 @@
 
 include 'db_connection.php';
 
+if(isset($_GET['id']) && ctype_digit($_GET['id']))
+{
+    $id=$_GET['id'];
+} else
+{
+    header('Location: select.php');
+}
+
 $name ='';
 $password='';
 $gender='';
@@ -13,26 +21,26 @@ if(isset($_POST['submit'])) {
 
         if (!isset($_POST['name']) || $_POST['name'] === '') {
             $ok = false;
-            error_log("There is something wrong!", 0);
+            error_log("There is something wrong!", 7);
         } else {
-            error_log("There is something wrong!", 1);
+            error_log("There is something wrong!", 8);
             $name = $_POST['name'];
         };
 
         if (!isset($_POST['password']) || $_POST['password'] === '') {
-            error_log("There is something wrong!", 2);
+            error_log("There is something wrong!", 9);
             $ok = false;
         } else {
             $password = $_POST['password'];
         };
         if (!isset($_POST['gender']) || $_POST['gender'] === '') {
-            error_log("There is something wrong!", 3);
+            error_log("There is something wrong!", 10);
             $ok = false;
         } else {
             $gender = $_POST['gender'];
         };
         if (!isset($_POST['color']) || $_POST['color'] === '') {
-            error_log("There is something wrong!", 4);
+            error_log("There is something wrong!", 11);
             $ok = false;
         } else {
             $color = $_POST['color'];
@@ -41,19 +49,30 @@ if(isset($_POST['submit'])) {
 
 
     if($ok) {
-
    $conn = OpenCon();
 
         $sql = sprintf(
-         "INSERT INTO users (name, gender, color) VALUES (
-         '%s', '%s', '%s')",
+         "UPDATE users SET name='%s', gender='%s', color='%s' WHERE id=%s",
             $conn ->real_escape_string($name),
             $conn ->real_escape_string($gender),
-            $conn ->real_escape_string($color));
+            $conn ->real_escape_string($color),
+        $id);
             $conn->query($sql);
-        error_log("There is something wrong!", 5);
-            echo "User added.";
-        error_log("There is something wrong!", 6);
+        error_log("There is something wrong!", 12);
+            echo "User updated.";
+        error_log("There is something wrong!", 13);
+            CloseCon($conn);
+    } else{
+
+        $conn = OpenCon();
+        $sql="SELECT * FROM users WHERE id=%s";
+        $result = $conn->query($sql);
+        foreach ($result as $row){
+            error_log("There is something wrong!", 14);
+            $name= $row['name'];
+            $gender= $row['gender'];
+            $color=$row['color'];
+        }
             CloseCon($conn);
     }
 }
